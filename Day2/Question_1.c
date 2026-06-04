713ee314ea9d0cdd72c8b4f97023cc5e2d25f8d6
GLB_25113CN021 <divyanshpandey101@gmail.com>
2026-06-04 15:19:00 +0530

    Update Question_1.c

--- Day2/Question_1.c
+++ Day2/Question_1.c
@@ -1,18 +1,16 @@
+/* Write a program to Find sum of digits of a number */ 
+
 #include <stdio.h>
 
 int main() {
     int number, digit, sum = 0;
-
     printf("Enter a number: ");
     scanf("%d", &number);
-
     while(number != 0) {
         digit = number % 10;
         sum = sum + digit;
         number = number / 10;
     }
-
     printf("Sum of digits = %d", sum);
-
     return 0;
 }
