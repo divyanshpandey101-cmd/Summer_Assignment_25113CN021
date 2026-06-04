a83af7bfca0ce4a5da2730998f77ae71d7482e24
GLB_25113CN021 <divyanshpandey101@gmail.com>
2026-06-04 15:16:43 +0530

    Update Question_3.c

--- Day1/Question_3.c
+++ Day1/Question_3.c
@@ -1,14 +1,14 @@
+/* Write a program to find Factorial of a number */
+
 #include<stdio.h>
 
 int main() {
     int num;
     long long factorial = 1;
 
-    // Input the number
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Calculate factorial
     for(int i = 1; i <= num; i++) {
         factorial *= i;
     }
