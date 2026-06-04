1dc935d9c23c02ba6f2679650a9ca59959a92503
GLB_25113CN021 <divyanshpandey101@gmail.com>
2026-06-04 13:12:30 +0530

    Update Question_3.c

--- Day4/Question_3.c
+++ Day4/Question_3.c
@@ -7,15 +7,13 @@ int result = 0;
 printf("Enter a number: ");    
 scanf("%d", &num);
     
-originalNum = num;
-    // Count the number of digits   
+originalNum = num;   
  while (originalNum != 0)
 {        
  digits++;        
  originalNum /= 10;    
 }
  originalNum = num;
-    // Calculate the sum of digits raised to the power of 'digits'   
  while (originalNum != 0) {       
  remainder = originalNum % 10;        
  result += (int)pow(remainder, digits);        
