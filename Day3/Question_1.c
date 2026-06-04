5ba8e2b46447447851bc3d37a278a25cb4eb3c96
GLB_25113CN021 <divyanshpandey101@gmail.com>
2026-06-04 15:29:08 +0530

    Update Question_1.c

--- Day3/Question_1.c
+++ Day3/Question_1.c
@@ -4,36 +4,26 @@
 
 int main() {
     int num, i, isPrime = 1;
-=======
-#include <stdio.h>
 
-int main() {
-    int num, i, prime = 1;
-    printf("Enter a number: ");
+    printf("Enter a positive integer: ");
     scanf("%d", &num);
 
     if (num <= 1) {
-        isPrime = 0;
-    } else {
-        for (i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                isPrime = 0;
-        prime = 0;
-    } else {
-        for (i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                prime = 0;
-                break;
-            }
+        printf("%d is not a prime number.\n", num);
+        return 0;
+    }
+
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            isPrime = 0;
+            break;
         }
     }
+
     if (isPrime)
-        printf("%d is a Prime Number.\n", num);
+        printf("%d is a prime number.\n", num);
     else
-        printf("%d is not a Prime Number.\n", num);
-    if (prime)
-        printf("%d is a Prime Number\n", num);
-    else
-        printf("%d is Not a Prime Number\n", num);
+        printf("%d is not a prime number.\n", num);
+
     return 0;
 }
