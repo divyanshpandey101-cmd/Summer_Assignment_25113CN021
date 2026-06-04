4c850ea0a919ea6ccf74a7cf8d94f5e186689caa
GLB_25113CN021 <divyanshpandey101@gmail.com>
2026-06-04 15:23:11 +0530

    Update Question_4.c

--- Day2/Question_4.c
+++ Day2/Question_4.c
@@ -1,23 +1,20 @@
+/* Write a program to Check whether a number is palindrome */
+
 #include <stdio.h>
 
 int main() {
     int number, original, reverse = 0, remainder;
-
     printf("Enter a number: ");
     scanf("%d", &number);
-
     original = number;
-
     while(number != 0) {
         remainder = number % 10;
         reverse = reverse * 10 + remainder;
         number = number / 10;
     }
-
     if(original == reverse)
         printf("Palindrome number");
     else
         printf("Not a palindrome number");
-
     return 0;
 }
