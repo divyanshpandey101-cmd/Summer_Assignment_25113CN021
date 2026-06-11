f21bcb7d519a52dec9b34e304bd7fb9596f2fb5d
GLB_25113CN021 <divyanshpandey101@gmail.com>
2026-06-11 12:28:08 +0530

    Update Question1.c

--- Day10/Question1.c
+++ Day10/Question1.c
@@ -8,11 +8,25 @@
 #include <stdio.h>
 
 void printChars(char ch, int count) {
-    if (count == 0)
+    if (count <= 0) {
         return;
+    }
     printf("%c", ch);
     printChars(ch, count - 1);
 }
 void printStarPyramid(int currentRow, int totalRows) {
-    if (currentRow > totalRows)
+    if (currentRow > totalRows) {
         return;
+    }
+    int spaces = totalRows - currentRow;
+    printChars(' ', spaces);
+    int stars = (2 * currentRow) - 1;
+    printChars('*', stars);
+    printf("\n");
+    printStarPyramid(currentRow + 1, totalRows);
+}
+int main() {
+    int totalRows = 5;
+    printStarPyramid(1, totalRows);
+    return 0;
+}
